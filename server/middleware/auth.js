949b797ceef2d0ce288c62219512b419000e4e8c
Symphonix Tech <symphonixtech@gmail.com>
2025-10-26 16:15:10 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -17,11 +17,14 @@ export const verifyToken = async (req, res, next) => {
 export const verifyAdmin = async (req, res, next) => {
   try {
     const token = req.cookies.admin;
+    console.log("token: ");
     console.log(token);
     if(!token) return res.status(401).json({ error: 'Not authenticated' });
     const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    console.log("Decoded Token: ");
     console.log(decoded)
     const user = await User.findById(decoded.id).select('-password');
+    console.log("User: ");
     console.log(user);
     if(user.role !== 'admin') return res.status(401).json({ error: 'Not authorized' });
     next();
